6160e042887ebeb27ab911c73d80888c023489fe
Christopher Jones <tkcwebdev@gmail.com>
2022-05-18 23:49:37 -0700

    1-project-installs: Install prettier and eslint config conjunction packages and formatted

--- site/.eslintrc.js
+++ site/.eslintrc.js
@@ -1,19 +1,31 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ['plugin:react/recommended', 'airbnb'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: ['react', '@typescript-eslint'],
-  rules: {
-    semi: ['error', 'always'],
-  },
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'plugin:react/recommended',
+		'airbnb',
+		'plugin:react/jsx-runtime',
+		'prettier',
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react', '@typescript-eslint'],
+	rules: {
+		semi: ['error', 'always'],
+		'react/jsx-filename-extension': [1, { extensions: ['.js', '.tsx'] }],
+		'react/function-component-definition': [
+			2,
+			{ namedComponents: 'arrow-function' },
+		],
+		'react/react-in-jsx-scope': 'off',
+		'react/jsx-props-no-spreading': 'off',
+	},
 };
