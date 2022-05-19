0f7d40d57d1b1abcce8f845cc023818c1cceae58
Christopher Jones <tkcwebdev@gmail.com>
2022-05-18 22:43:49 -0700

    1-project-installs: Add prettier and format everything

--- site/.eslintrc.js
+++ site/.eslintrc.js
@@ -3,10 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
+  extends: ['plugin:react/recommended', 'airbnb'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
@@ -15,10 +12,7 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
+  plugins: ['react', '@typescript-eslint'],
   rules: {
     semi: ['error', 'always'],
   },
