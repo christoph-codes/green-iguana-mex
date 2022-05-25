c5970eb6f400bb677501dcdb93b534cc1c4c933f
Christopher Jones <tkcwebdev@gmail.com>
2022-05-24 17:29:56 -0700

    14-header-component: Create header component

---
+++ site/components/Header/Header.tsx
@@ -0,0 +1,83 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { FC } from 'react';
+import { IoCloseCircle } from 'react-icons/io5';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import greenIguanaLogo from '../../assets/images/green-iguana-logo.png';
+import styles from './Header.module.scss';
+import Container from '../Container';
+import Button from '../Button';
+import { toastTabLink } from '../../util/navLinks';
+// eslint-disable-next-line import/no-unresolved
+import { useSideNav } from '../../providers/SideNavProvider';
+
+export type TLink = {
+	label: string;
+	path: string;
+	newTab?: boolean;
+	icon?: string;
+};
+
+export type THeaderProps = {
+	sticky?: boolean;
+	links?: TLink[];
+	className?: string;
+};
+
+const Header: FC<THeaderProps> = ({ links, sticky, className }) => {
+	const { isOpen, toggleSideNav } = useSideNav();
+	return (
+		<header
+			className={`${styles.Header} ${className} ${
+				sticky ? styles.Header__sticky : ''
+			}`}
+		>
+			<Container className={styles.Header__container}>
+				<Link href="/">
+					<span className={styles.Header__container__logo}>
+						<Image src={greenIguanaLogo} alt="Green Iguana Logo" />
+					</span>
+				</Link>
+				<nav className={`${styles.Header__container__nav}`}>
+					{links?.map((link: TLink, index: number) => (
+						<li
+							className={styles.Header__container__nav__link}
+							key={index}
+						>
+							<Link
+								target={link.newTab ? '_blank' : '_self'}
+								href={link.path}
+							>
+								{/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+								<a>
+									{link.icon && link.icon}
+									{link.label}
+								</a>
+							</Link>
+						</li>
+					))}
+					<li
+						className={
+							styles[`Header__container__nav__link--button`]
+						}
+					>
+						<Button href={toastTabLink}>Order Online</Button>
+					</li>
+				</nav>
+				<button
+					type="button"
+					onClick={() => toggleSideNav()}
+					className={styles.Header__container__hamburger}
+				>
+					{!isOpen ? (
+						<GiHamburgerMenu size={32} />
+					) : (
+						<IoCloseCircle size={32} />
+					)}
+				</button>
+			</Container>
+		</header>
+	);
+};
+
+export default Header;
