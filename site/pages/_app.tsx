884cd0aae39331b659d0db979f380b121b84776b
Christopher Jones <tkcwebdev@gmail.com>
2022-05-24 17:31:11 -0700

    14-header-component: Add side nav provider to the app

--- site/pages/_app.tsx
+++ site/pages/_app.tsx
@@ -1,9 +1,13 @@
 import '../styles/globals.scss';
 import 'animate.css';
 import type { AppProps } from 'next/app';
+// eslint-disable-next-line import/no-unresolved
+import SideNavProvider from '../providers/SideNavProvider';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-	<Component {...pageProps} />
+	<SideNavProvider>
+		<Component {...pageProps} />
+	</SideNavProvider>
 );
 
 export default MyApp;
