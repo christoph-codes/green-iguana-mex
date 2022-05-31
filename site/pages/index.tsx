6408a0cae5823c06cb0475325c1bd365ae8028ea
Christopher Jones <tkcwebdev@gmail.com>
2022-05-30 23:51:35 -0700

    19-home-page: Add remaining styles to homepage plus additional sections with content

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -3,8 +3,6 @@ import Container from '../components/Container';
 import Section from '../components/Section';
 import Row from '../components/Row';
 import Button from '../components/Button';
-import Footer from '../components/Footer';
-import iguana from '../assets/images/iguana.svg';
 import MenuItem from '../components/MenuItem';
 import PageTemplate from '../templates/Page';
 import greenIguanaSEO from '../assets/images/gim-seo_image.png';
@@ -26,7 +24,7 @@ const Home: NextPage = (): any => (
 			className={`${styles.Home__hero} 🔥bg-primary`}
 			bgImg={homeHero}
 		>
-			<Row columns={{ xs: [8, 4] }} align="center" noGap>
+			<Row columns={{ xs: [12, 12], md: [8, 4] }} align="center" noGap>
 				<article className={styles.Home__hero__text}>
 					<h1>Welcome Home</h1>
 					<p>
@@ -51,7 +49,32 @@ const Home: NextPage = (): any => (
 				</article>
 			</Row>
 		</Section>
-		<Container>
+		<div className={styles.Home__actions}>
+			<Container>
+				<Row>
+					<p>
+						850 S Bluff Street
+						<br />
+						St. George, UT 84770
+					</p>
+					<p>
+						Tuesday - Saturday @ 11am - 9pm <br />
+						Sunday @ 11am - 8pm
+					</p>
+					<p>
+						Phone Number <br />
+						435-628-4863
+					</p>
+				</Row>
+			</Container>
+		</div>
+		<Section
+			className={`${styles.Home__hero} 🔥bg-offwhite`}
+			title="Green Iguana Plato Especiales"
+			description="All plates served with Rice, Beans, Lettuce, Pico, Guacomole,
+			Sour Cream, and Corn or Flour Tortillas **Do not include
+			Tortillas"
+		>
 			<MenuItem
 				category="NEW"
 				title="Green Iguana Plato Especiales**"
@@ -71,128 +94,14 @@ const Home: NextPage = (): any => (
 				Guacomole | Sour Cream | Rice | Fresh Veggies."
 				price="12.99"
 			/>
-		</Container>
-		<Footer
-			img={iguana}
-			links={[
-				{ label: 'Menu', path: '/menu' },
-				{ label: 'Catering', path: '/catering' },
-				{ label: 'Locations', path: '/locations' },
-				{ label: 'Order Online', path: '/order-online' },
-				{ label: 'Contact', path: '/contact' },
-			]}
-		/>
-
-		<Section bgColor="offwhite">
-			<h3>Hello Bro!</h3>
-			<h1 className="animate__animated animate__bounce">
-				Welcome to Green Iguana
-			</h1>
-			<p>
-				Get started by editing this block of code over and over but I
-				have to make sure that everything is good before I approve of
-				this font size.{' '}
-				<code className={styles.code}>pages/index.tsx</code>
-				<a href="https://nextjs.org">
-					Next.js! Longer title to see how it wraps
-				</a>
-			</p>
+			<Button
+				href="/menu"
+				className="🔥mx-auto 🔥mt-2"
+				variant="secondary"
+			>
+				View Menu
+			</Button>
 		</Section>
-		<Section title="Hello Bro!" bgColor="offwhite" className="🔥pt-0">
-			<Row align="center">
-				<div className="🔥bg-primary">
-					<p>Hello</p>
-				</div>
-				<div className="🔥bg-tertiary">
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit.
-						Cum odit voluptatem nulla tempore? Praesentium vitae
-						laborum rem eius. Repudiandae ipsum tenetur dolorum
-						earum saepe vero recusandae excepturi consectetur fugit
-						modi.
-					</p>
-				</div>
-				<div className="🔥bg-secondary">
-					<p>Hello</p>
-				</div>
-			</Row>
-		</Section>
-
-		<main className="🔥bg-offwhite">
-			<Container>
-				<div className="🔥d-md-flex 🔥d-block 🔥align-center 🔥justify-center">
-					<h2 className="animate__animated animate__bounce 🔥ml-auto 🔥p-2 🔥bg-primary 🔥bg-md-tertiary 🔥bg-lg-secondary 🔥text-white">
-						Welcome to Green Iguana
-					</h2>
-					<Row>
-						<Button>Primary!</Button>
-						<Button variant="primary-outline">
-							Primary Outline!
-						</Button>
-						<Button variant="secondary">Secondary!</Button>
-						<Button variant="secondary-outline">
-							Secondary Outline!
-						</Button>
-						<Button variant="tertiary">Tertiary!</Button>
-						<Button variant="tertiary-outline">
-							Tertiary Outline!
-						</Button>
-						<Button fullWidth>FullWidth!</Button>
-						<Button href="/contact" fullWidth>
-							HREF!
-						</Button>
-						<Button fullWidth>Primary!</Button>
-					</Row>
-					<ul style={{ width: '50%' }} className="🔥text-sm-tertiary">
-						<li>Hello bro</li>
-						<li>Hello gal</li>
-						<li>Hello mama</li>
-						<ul>
-							<li>Hello dada</li>
-							<li>Hello sister</li>
-						</ul>
-						<li>Hello grandma</li>
-					</ul>
-				</div>
-				<h3 className="animate__animated animate__bounce">
-					Welcome to Green Iguana
-				</h3>
-				<p style={{ width: '50%' }} className="🔥text-right">
-					Get started by editing this block of code over and over but
-					I have to make sure that everything is good before I approve
-					of this font size.{' '}
-					<code className={styles.code}>pages/index.tsx</code>
-				</p>
-				<h4 className="animate__animated animate__bounce 🔥my-6">
-					Welcome to Green Iguana
-				</h4>
-				<p style={{ width: '50%' }} className="🔥px-4 🔥text-center">
-					Get started by editing this block of code over and over but
-					I have to make sure that everything is good before I approve
-					of this font size.{' '}
-					<code className={styles.code}>pages/index.tsx</code>
-				</p>
-				<h5 className="animate__animated animate__bounce 🔥text-tertiary">
-					Welcome to Green Iguana
-				</h5>
-				<p style={{ width: '50%' }} className={styles.description}>
-					Get started by editing this block of code over and over but
-					I have to make sure that everything is good before I approve
-					of this font size.{' '}
-					<code className={styles.code}>pages/index.tsx</code>
-				</p>
-				<h6 className="animate__animated animate__bounce">
-					Welcome to Green Iguana
-				</h6>
-
-				<p style={{ width: '50%' }} className="🔥text-primary">
-					Get started by editing this block of code over and over but
-					I have to make sure that everything is good before I approve
-					of this font size.{' '}
-					<code className={styles.code}>pages/index.tsx</code>
-				</p>
-			</Container>
-		</main>
 	</PageTemplate>
 );
 
