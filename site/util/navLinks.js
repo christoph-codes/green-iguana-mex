ea1c54ce8ea09a457bd16929c01f12d1ef5305ea
Christopher Jones <tkcwebdev@gmail.com>
2022-06-03 23:05:09 -0700

    46-category-tabs: Add gift card link to the main url

--- site/util/navLinks.js
+++ site/util/navLinks.js
@@ -1,12 +1,16 @@
 import { FaFacebookF, FaTwitter } from 'react-icons/fa';
 
-export const toastTabLink = `https://www.toasttab.com/greeniguanastg/v3`;
+export const toastTabLink = `https://www.toasttab.com/greeniguanastg/v3?utmCampaign=website`;
 
 const navLinks = [
 	{ label: 'Home', path: '/' },
 	{ label: 'Menu', path: '/menu' },
 	{ label: 'Catering', path: '/catering' },
 	{ label: 'Locations', path: '/locations' },
+	{
+		label: 'Gift Cards',
+		path: 'http://www.toasttab.com/greeniguanastg/giftcards?utmCampaign=website',
+	},
 	{
 		path: 'https://facebook.com/green-iguana',
 		icon: <FaFacebookF size={32} />,
