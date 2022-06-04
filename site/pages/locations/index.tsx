599d54dc8a8e1c0040a85ee1aba56daa6ba52b73
Christopher Jones <tkcwebdev@gmail.com>
2022-06-04 01:09:46 -0700

    21-locations-page: Fix breakpoint for map on the locations page

--- site/pages/locations/index.tsx
+++ site/pages/locations/index.tsx
@@ -20,7 +20,7 @@ const LocationsPage: NextPage = (): any => (
 			title="Locations"
 			description="View our primary location with more locations opening soon!"
 		>
-			<Row align="center" columns={{ xs: [12, 12], sm: [9, 3] }}>
+			<Row align="center" columns={{ xs: [12, 12], md: [9, 3] }}>
 				<iframe
 					title="Green Iguana Mexican Restaurant in St George Utah"
 					src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7108.177449767559!2d-113.59171151398186!3d37.09271962739878!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80ca452644ec86f9%3A0xbf44cc0e2fdbc192!2sGreen%20Iguana%20Mexican%20Restaurant!5e0!3m2!1sen!2sus!4v1654327431108!5m2!1sen!2sus"
