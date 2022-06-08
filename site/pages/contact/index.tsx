42a1ab4d539b2688c19a8aeef5100986b542dad7
Christopher Jones <tkcwebdev@gmail.com>
2022-06-07 22:28:21 -0700

    23-contact-page: Add custom form to the contact page

--- site/pages/contact/index.tsx
+++ site/pages/contact/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next';
 import PageTemplate from '../../templates/Page';
 import Section from '../../components/Section';
 import styles from './ContactPage.module.scss';
+import Form from '../../components/Form';
 
 const ContactPage: NextPage = (): any => (
 	<PageTemplate
@@ -13,8 +14,49 @@ const ContactPage: NextPage = (): any => (
 		}}
 		className={styles.Contact}
 	>
-		<Section title="Contact">
-			<p>Contact Content will go here.</p>
+		<Section
+			title="Contact"
+			description="Contact Green Iguana Mexican Restaurant today. We look forward to hearing from you!"
+		>
+			<Form
+				submitButton={{
+					children: 'Submit',
+				}}
+				inputs={[
+					{
+						type: 'text',
+						name: 'fname',
+						placeholder: 'John',
+						label: 'First Name',
+						required: true,
+						validation: ['REQUIRED'],
+					},
+					{
+						type: 'text',
+						name: 'lname',
+						placeholder: 'Doe',
+						label: 'Last Name',
+						required: true,
+						validation: ['REQUIRED'],
+					},
+					{
+						type: 'email',
+						name: 'email',
+						placeholder: 'john@doe.com',
+						label: 'Email',
+						required: true,
+						validation: ['REQUIRED', 'EMAIL'],
+					},
+					{
+						type: 'textarea',
+						name: 'message',
+						placeholder: 'Let us know how we are doing!',
+						label: 'Message',
+						required: true,
+						validation: ['REQUIRED'],
+					},
+				]}
+			/>
 		</Section>
 	</PageTemplate>
 );
