9bc3a52dfef7d1cff2266555eac1325b089a6a62
Christopher Jones <tkcwebdev@gmail.com>
2022-06-12 02:18:37 -0700

    49-job-listing-template: Created job listing dynamic page route that uses the page template

---
+++ site/pages/careers/[joblisting].tsx
@@ -0,0 +1,24 @@
+import { useRouter } from 'next/router';
+import JobListing from '../../templates/JobListing';
+import jobListings from '../../util/joblistings';
+
+const JobPage = () => {
+	const router = useRouter();
+	const { joblisting } = router.query;
+	const job = jobListings.find((j) => j.slug === joblisting);
+	if (job) {
+		return (
+			<JobListing
+				metaData={{ title: job.title, description: job.description }}
+				title={job.title}
+				location={job.location}
+				pay={job.pay}
+			>
+				Job: {joblisting}
+			</JobListing>
+		);
+	}
+	return null;
+};
+
+export default JobPage;
