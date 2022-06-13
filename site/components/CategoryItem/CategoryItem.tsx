4b607ac5b664d2746527e1ef85652d97cb40fa0e
Christopher Jones <tkcwebdev@gmail.com>
2022-06-12 22:23:59 -0700

    53-update-homepage-plate-specials: Create category item component to display category menu content

---
+++ site/components/CategoryItem/CategoryItem.tsx
@@ -0,0 +1,40 @@
+import { FC } from 'react';
+import Section from '../Section';
+import MenuItem from '../MenuItem';
+import styles from './CategoryItem.module.scss';
+import { TMenuGroup } from '../Tabs/Tabs';
+
+export type TCategoryItemProps = {
+	category: TMenuGroup;
+	className?: string;
+	rest?: any[];
+};
+
+const CategoryItem: FC<TCategoryItemProps> = ({ category }) => (
+	<Section className={`${styles.CategoryItem}`}>
+		{category.title && (
+			<h3
+				className={`${styles.CategoryItem__title} 🔥text-center 🔥mb-1`}
+			>
+				{category.title}
+			</h3>
+		)}
+		{category.description && (
+			<div
+				className={`${styles.CategoryItem__description} p 🔥text-center 🔥mb-2 🔥pb-2`}
+			>
+				{category.description}
+			</div>
+		)}
+		<div className={styles.CategoryItem__columns}>
+			{category.content?.map((foodCategory: any) => (
+				<MenuItem
+					className={styles.CategoryItem__columns__column}
+					{...foodCategory}
+				/>
+			))}
+		</div>
+	</Section>
+);
+
+export default CategoryItem;
