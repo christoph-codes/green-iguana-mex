465ea783cb07979c908c091edbb0615444344e48
Christopher Jones <tkcwebdev@gmail.com>
2022-06-14 20:06:17 -0700

    23-contact-page: Use an enum to define the error messages for the input validations

--- site/util/inputValidations.ts
+++ site/util/inputValidations.ts
@@ -1,8 +1,27 @@
+export enum EErrorMessages {
+	'REQUIRED' = 'This field is required',
+	'OVERFIVE' = 'Value must be over five characters',
+	'EMAIL' = 'Please enter a valid email',
+}
 // All functions must either return true or the error message in which failed.
 const inputValidations = {
 	REQUIRED(value: any) {
 		if (value === '') {
-			return 'This field is required';
+			return EErrorMessages.REQUIRED;
+		} 
+		return true;
+	},
+	OVERFIVE(value: any) {
+		if (value?.length < 5) {
+			return EErrorMessages.OVERFIVE;
+		} 
+		return true;
+	},
+	EMAIL(value: any) {
+		const pattern = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
+		console.log('pattern', pattern.test(value));
+		if (!pattern.test(value)) {
+			return EErrorMessages.EMAIL;
 		} 
 		return true;
 	},
