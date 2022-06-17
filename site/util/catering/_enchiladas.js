9d2599f1c62669cce452eeb9360ad7966dc11bfe
Christopher Jones <tkcwebdev@gmail.com>
2022-06-17 00:03:32 -0700

    main: Add catering menu

---
+++ site/util/catering/_enchiladas.js
@@ -0,0 +1,20 @@
+const enchiladas = {
+	title: 'Enchiladas (ALC)',
+	description: '*Enchiladas include Sour Cream',
+	content: [
+		{
+			size: '1/2 Pan',
+			qty: '10',
+			price: '$36.99',
+			description: '1 **meat choice & 1 sauce choice',
+		},
+		{
+			size: 'Full Pan',
+			qty: '20',
+			price: '$69.99',
+			description: 'Choose 1-2 **meats & 1-2 sauce(s)',
+		},
+	],
+};
+
+export default enchiladas;
---
+++ site/util/catering/_miniBurritos.js
@@ -0,0 +1,21 @@
+const miniBurritos = {
+	title: 'Mini Burritos (ALC)',
+	description:
+		'*Rice, Pico de Gallo, & cheese inside burrito with more cheese & sauce on top',
+	content: [
+		{
+			size: '1/2 Pan Mini Burritos',
+			qty: '8',
+			price: '$44.99',
+			description: '1 **meat choice & 1 sauce choice',
+		},
+		{
+			size: 'Full Pan Mini Burritos',
+			qty: '16',
+			price: '$79.99',
+			description: 'Choose 1-2 **meats & 1-2 sauce(s)',
+		},
+	],
+};
+
+export default miniBurritos;
