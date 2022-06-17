c1663e628137eb51feb75b04f94f62fb68cc5f43
Christopher Jones <tkcwebdev@gmail.com>
2022-06-17 00:03:47 -0700

    main: Create card component

---
+++ site/components/Card/Card.tsx
@@ -0,0 +1,37 @@
+import Image, { StaticImageData } from 'next/image';
+import { FC } from 'react';
+import styles from './Card.module.scss';
+
+export type TCardProps = {
+	className?: string;
+	children?: any;
+	title?: string;
+	description?: string;
+	image?: StaticImageData;
+	rest?: any[];
+};
+
+const Card: FC<TCardProps> = ({
+	className,
+	children,
+	title,
+	description,
+	image,
+	rest,
+}) => {
+	console.log('Card');
+	return (
+		<div className={`${styles.Card} ${className}`} {...rest}>
+			{image && (
+				<Image className={styles.Card__image} src={image} alt={title} />
+			)}
+			<div className={styles.Card__content}>
+				{title && <h4>{title}</h4>}
+				{description && <p>{description}</p>}
+				{children}
+			</div>
+		</div>
+	);
+};
+
+export default Card;
