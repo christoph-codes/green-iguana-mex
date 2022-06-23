1ab6ffb2caeee48cf0970246bbdf236cce1451d4
Christopher Jones <tkcwebdev@gmail.com>
2022-06-23 01:39:27 -0700

    23-contact-page: Add form prop to the input to pass in for updating the form state via the input

--- site/components/Input/Input.tsx
+++ site/components/Input/Input.tsx
@@ -1,5 +1,4 @@
-import { FC } from 'react';
-import { useForm } from '../../providers/FormContext';
+import { ChangeEvent, FC } from 'react';
 import { EErrorMessages } from '../../util/inputValidations';
 import styles from './Input.module.scss';
 
@@ -10,6 +9,18 @@ export type TInputProps = {
 	label: string;
 	required: boolean;
 	validation: EErrorMessages[];
+	form: {
+		[name: string]: {
+			value: any;
+			isNotValid: string[];
+		};
+	};
+	onChange: (
+		// eslint-disable-next-line no-unused-vars
+		e: ChangeEvent<HTMLInputElement>,
+		// eslint-disable-next-line no-unused-vars
+		validation: EErrorMessages[]
+	) => void;
 };
 
 const Input: FC<TInputProps> = ({
@@ -19,56 +30,55 @@ const Input: FC<TInputProps> = ({
 	label,
 	required = false,
 	validation,
-}) => {
-	const { form, formUpdate } = useForm();
-	return (
-		<label className={`${styles.Input}`} htmlFor={name}>
-			<>
-				<span
-					className={`${styles.Input__label} ${
-						form[name as keyof typeof form]?.isNotValid.some(
-							(e) => e !== ''
-						)
-							? styles['Input__label--error']
-							: ''
-					}`}
-				>
-					{label}
-				</span>
-				<input
-					className={`${styles.Input__input} ${
-						form[name as keyof typeof form]?.isNotValid.some(
-							(e) => e !== ''
-						)
-							? styles['Input__input--error']
-							: ''
-					}`}
-					name={name}
-					placeholder={placeholder}
-					type={type}
-					id={name}
-					value={form[name as keyof typeof form]?.value || ''}
-					onChange={(e) => formUpdate(e, validation)}
-					required={required}
-				/>
-				{form[name as keyof typeof form]?.isNotValid.length > 0 && (
-					<ul className={styles.Input__errors}>
-						{form[name as keyof typeof form]?.isNotValid.map(
-							(err, index) =>
-								err !== '' && (
-									<li
-										key={index}
-										className={styles.Input__errors__error}
-									>
-										{err}
-									</li>
-								)
-						)}
-					</ul>
-				)}
-			</>
-		</label>
-	);
-};
+	form,
+	onChange,
+}) => (
+	<label className={`${styles.Input}`} htmlFor={name}>
+		<>
+			<span
+				className={`${styles.Input__label} ${
+					form[name as keyof typeof form]?.isNotValid.some(
+						(e) => e !== ''
+					)
+						? styles['Input__label--error']
+						: ''
+				}`}
+			>
+				{label}
+			</span>
+			<input
+				className={`${styles.Input__input} ${
+					form[name as keyof typeof form]?.isNotValid.some(
+						(e) => e !== ''
+					)
+						? styles['Input__input--error']
+						: ''
+				}`}
+				name={name}
+				placeholder={placeholder}
+				type={type}
+				id={name}
+				value={form[name as keyof typeof form]?.value || ''}
+				onChange={(e) => onChange(e, validation)}
+				required={required}
+			/>
+			{form[name as keyof typeof form]?.isNotValid.length > 0 && (
+				<ul className={styles.Input__errors}>
+					{form[name as keyof typeof form]?.isNotValid.map(
+						(err, index) =>
+							err !== '' && (
+								<li
+									key={index}
+									className={styles.Input__errors__error}
+								>
+									{err}
+								</li>
+							)
+					)}
+				</ul>
+			)}
+		</>
+	</label>
+);
 
 export default Input;
