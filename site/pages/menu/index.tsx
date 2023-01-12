03f9436c7a3f9c83ab9f2ce97abec9d8c9c59f38
Christopher Jones <tkcwebdev@gmail.com>
2023-01-11 17:23:07 -0800

    91: Add new menu to accomodate changes

--- site/pages/menu/index.tsx
+++ site/pages/menu/index.tsx
@@ -2,9 +2,11 @@ import type { NextPage } from 'next';
 import PageTemplate from '../../templates/Page';
 import Section from '../../components/Section';
 import styles from './MenuPage.module.scss';
-import Tabs from '../../components/Tabs';
-import menu from '../../util/menu';
+// import Tabs from '../../components/Tabs';
+// import menu from '../../util/menu';
 import menuImg from '../../assets/images/gim-menu2.jpg';
+import { newMenuOptions } from '../../util/menu/newMenu';
+import MenuItem from '../../components/MenuItem';
 
 const MenuPage: NextPage = (): any => (
 	<PageTemplate
@@ -16,8 +18,32 @@ const MenuPage: NextPage = (): any => (
 		className={styles.Menu}
 	>
 		<Section bgImg={menuImg} className={styles.Menu__hero} />
-		<Section className="🔥pt-2 🔥pb-1" title="Menu">
-			<Tabs data={menu} />
+		<Section className="🔥pt-2 🔥pb-1">
+			{/* <Tabs data={menu} /> */}
+			<h1 className={styles.Menu__title}>
+				Menu<span>NEW!</span>
+			</h1>
+			<p className="🔥text-center 🔥py-2">
+				Check out our new menu as we have updated to include all of our
+				popular and well known specials!
+			</p>
+			{newMenuOptions.map((step, idx) => (
+				<div key={idx} className={styles.Menu__step}>
+					<h3>{step.title}</h3>
+					{step.description && <p>{step.description}</p>}
+					<div className={styles.Menu__options}>
+						{step.options.map((opt, index) => (
+							<MenuItem
+								className={styles.Menu__options__option}
+								key={index}
+								title={opt.title}
+								description={opt.description}
+								price={opt.price}
+							/>
+						))}
+					</div>
+				</div>
+			))}
 		</Section>
 	</PageTemplate>
 );
