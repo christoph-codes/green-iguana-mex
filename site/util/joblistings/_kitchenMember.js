a8ec1f2b1e5f455de872cf2893c1d811ce79fe78
christoph-codes <tkcwebdev@gmail.com>
2023-01-22 22:30:26 -0800

    93: Update kitchen member description to not have filler text

--- site/util/joblistings/_kitchenMember.js
+++ site/util/joblistings/_kitchenMember.js
@@ -2,7 +2,7 @@ const kitchenMember = {
 	slug: 'kitchen-member',
 	title: 'Restaurant Kitchen Team Member',
 	description:
-		'Description goes here. Green Iguana is looking to hire energetic Team Members to join our  #greeniguanafamily.',
+		'Green Iguana is looking to hire energetic Team Members to join our  #greeniguanafamily.',
 	location: 'St. George, UT',
 	pay: 'Starting at $ 14.00/ hour!',
 };
