c2e3a538ea2aaaf9cbc5900d175c74fc14529000
christoph-codes <tkcwebdev@gmail.com>
2023-01-26 12:31:00 -0800

    95: Create global address and hours for easy updates

---
+++ site/util/content.tsx
@@ -0,0 +1,17 @@
+export const toastTabLink = `https://www.toasttab.com/greeniguanastg/v3?utmCampaign=website`;
+
+export const address = (
+	<>
+		250 Red Cliffs Drive #14A
+		<br />
+		St.George, UT 84790
+	</>
+);
+
+export const hours = (
+	<>
+		Sunday @ 11am - 9pm
+		<br />
+		Monday-Saturday @ 11am-9:30PM
+	</>
+);
